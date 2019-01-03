8623a47dc44bc9984da1b66941f6544823588b4b
Karim Said <karim@egyptiankarim.com>
2019-01-03 11:52:50 -0500

    Tweaked asset precompilation for compatability with Heroku.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,17 +3,23 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path
+# Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# For the glyphs!
+Rails.application.config.assets.precompile += ['glyphicons-halflings-regular.*']
 
 # To support controller specific css/js rendering
 Rails.application.config.assets.precompile += ['*.css']
+Rails.application.config.assets.precompile += ['*.scss']
 Rails.application.config.assets.precompile += ['*.js']
 
 # All my lib images
 Rails.application.config.assets.precompile += ['*.png']
-Rails.application.config.assets.precompile += ['*.ico']
