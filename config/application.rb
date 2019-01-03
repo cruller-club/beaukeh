c47d8e4a215cb21bc8750a20ac40ecb4fb2612b0
Karim Said <karim@egyptiankarim.com>
2019-01-03 13:53:08 -0500

    Major Rails upgrade.

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Beaukeh
   class Application < Rails::Application
-    config.app_name = "beaukeh"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
