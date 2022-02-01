6d2048e77a9f7feeeeadce20dc1ed77efdb7355b
Karim Said <karim@egyptiankarim.com>
2022-02-01 11:49:04 -0500

    Taking out mint route.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'about', to: 'static#about', as: :about
   
   get 'static', to: 'static#index', as: :index
-  get 'mint', to: 'static#mint', as: :mint
+  # get 'mint', to: 'static#mint', as: :mint
   get 'static/:background_color', to: 'static#index', as: :background
   get 'gimme/:signature', to: 'static#gimme', as: :gimme
   get 'show/:signature', to: 'static#show', as: :show
